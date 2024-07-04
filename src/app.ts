4e394be3bacdf045f8d4a0265914a62b674f1ad9
mamun2519 <juborajislam46@gmail.com>
2024-07-04 09:19:47 +0600

    add to middelware

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { StatusCodes } from "http-status-codes";
+import { ApplicationRoute } from "./app/routes";
+import globalErrorHandler from "./app/middleware/globalErrorHandler";
+
 const app: Application = express();
+
+app.use([
+  cookieParser(),
+  express.json(),
+  express.urlencoded({ extended: true }),
+]);
