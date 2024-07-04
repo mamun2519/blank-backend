d51095e4853794bd4520633f977d8150be2bf1f9
mamun2519 <juborajislam46@gmail.com>
2024-07-04 09:22:44 +0600

    add to middleware

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,26 @@ import cookieParser from "cookie-parser";
 import { StatusCodes } from "http-status-codes";
 import { ApplicationRoute } from "./app/routes";
 import globalErrorHandler from "./app/middleware/globalErrorHandler";
-
+import helmet from "helmet";
 const app: Application = express();
-
+//* middleware--------
 app.use([
   cookieParser(),
   express.json(),
   express.urlencoded({ extended: true }),
 ]);
+
+//*This Middleware help to Cross-Site Scripting(XSS) Protection
+// app.use(helmet.xssFilter());
+
+// //* Request Limitation
+// app.use(RequestLimit);
+// Application Route---------------
+app.use("/api/v1", ApplicationRoute);
+
+//Root Route-----------
+app.get("/", (req: Request, res: Response) => {
+  res.status(StatusCodes.OK).send({
+    StatusCodes: 404,
+  });
+});
